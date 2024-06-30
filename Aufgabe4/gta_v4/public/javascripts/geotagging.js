b60ccab04a62083922cb6af237afa4e70683fa26
Hugo Dünger <hugo.duenger@gmail.com>
2024-06-30 22:00:25 +0200

    use ajax for client updates

--- Aufgabe4/gta_v4/public/javascripts/geotagging.js
+++ Aufgabe4/gta_v4/public/javascripts/geotagging.js
@@ -7,6 +7,7 @@
 // "console.log" writes to the browser's console. 
 // The console window must be opened explicitly in the browser.
 // Try to find this output in the browser...
+
 console.log("The geoTagging script is going to start...");
 
 /**
@@ -29,6 +30,24 @@ function writeLocation(location) {
     mapManager.initMap(location.latitude, location.longitude);
     mapManager.updateMarkers(location.latitude, location.longitude);
 }*/
+let searchterm = "";
+let latitude = "";
+let longitude = "";
+let name = "";
+let hashtag = "";
+let mapManager = undefined;
+
+function readInputDiscovery() {
+    this.searchterm = document.getElementById("search-input").value;
+    this.latitude = document.getElementById("discovery-latitude").value;
+    this.longitude = document.getElementById("discovery-longitude").value;
+}
+
+function readInputTagging() {
+    readInputDiscovery();
+    this.name = document.getElementById("name-input").value;
+    this.hashtag = document.getElementById("hashtag-input").value;
+}
 
 function updateLocation() {
     if (!isLocationSet()) {
@@ -55,13 +74,63 @@ function writeLocation(location) {
     document.getElementById("discovery-longitude").value = location.longitude;
     document.getElementById("mapView").remove();
     document.getElementById("map-descr").remove();
-    let mapManager = new MapManager();
-    mapManager.initMap(location.latitude, location.longitude);
+    if (this.mapManager === undefined) {
+        this.mapManager = new MapManager();
+    }
+    this.mapManager.initMap(location.latitude, location.longitude);
     //mapManager.updateMarkers(location.latitude, location.longitude);
-    console.log(document.getElementById("map").dataset.tags);
     let element = document.getElementById("map");
-    console.log(element);
-    mapManager.updateMarkers(location.latitude, location.longitude, JSON.parse(element.dataset.tags || "[]"));
+    this.mapManager.updateMarkers(location.latitude, location.longitude, JSON.parse(element.dataset.tags || "[]"));
+}
+
+function updateMapMarkers(taglist) {
+    this.mapManager.updateMarkers(latitude, longitude, taglist);
+}
+
+document.getElementById("search-submit").addEventListener("click", function (e) {
+    e.preventDefault();
+    searchEvent();
+})
+
+function searchEvent() {
+    readInputDiscovery();
+    fetchResults().then(result => result.json()).then((data) => {
+        updateSearch(data);
+    });
+}
+
+document.getElementById("add-tag-submit").addEventListener("click", function (e) {
+    e.preventDefault();
+    readInputTagging();
+    addTag().then(() => {
+        searchEvent();
+    })
+});
+
+async function addTag() {
+    return fetch("http://localhost:3000/api/geotags", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({"name": this.name, "hashtag": this.hashtag, "latitude": this.latitude, "longitude": this.longitude})
+    })
+}
+
+async function fetchResults() {
+    return fetch("http://localhost:3000/api/geotags/?" + new URLSearchParams({
+        searchterm: this.searchterm,
+        latitude: this.latitude,
+        longitude: this.longitude
+    }).toString(), {
+        method: "GET",
+        headers: {"Content-Type": "application/json"}
+    });
+}
+
+function updateSearch(taglist) {
+    document.getElementById("discoveryResults").innerHTML = ejs.render(`<% if (taglist !== undefined) taglist.forEach(gtag => { %>
+                        <li><%= gtag.name %> ( <%= gtag.latitude %>,<%= gtag.longitude %>) <%= gtag.hashtag %> </li>
+                    <% }); %>`, {taglist: taglist});
+    updateMapMarkers(taglist);
 }
 
 // Wait for the page to fully load its DOM content, then call updateLocation
